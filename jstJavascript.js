0af480530d8913052039385e03f28105e3e45c0c
Nedu Ibezim <Ibezim.5@osu.edu>
2023-03-20 23:57:51 -0400

    Mach 0

--- jstJavascript.js
+++ jstJavascript.js
@@ -1,88 +1,119 @@
-// Two ArrayLists to store the vertices for two shapes
-// This example assumes that each shape will have the same
-// number of vertices, i.e. the size of each ArrayList will be the same
-let circle = [];
-let square = [];
+window.addEventListener("DOMContentLoaded", function () {
+  var circles = document.querySelectorAll(".circle");
+  circles.forEach(function (circle) {
+    circle.style.backgroundColor = getRandomColor();
+  });
+});
 
-// An ArrayList for a third set of vertices, the ones we will be drawing
-// in the window
-let morph = [];
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  // for (var i = 0; i < 6; i++) {
 
-// This boolean variable will control if we are morphing to a circle or square
-let state = false;
+  color += letters[Math.floor(Math.random() * 16)];
+  color += letters[Math.floor(Math.random() * 16)];
+  color += letters[Math.floor(Math.random() * 16)];
+  // color += letters[Math.floor(Math.random() * 16)];
+  // color += letters[Math.floor(Math.random() * 16)];
+  // color += letters[Math.floor(Math.random() * 16)];
+  //}
+  return color;
+}
+
+// function setup() {
+//   createCanvas(800, 800);
+// }
+// var letters = "0123456789ABCDEF";
+// var color = "#";
+// color += letters[Math.floor(Math.random() * 16)];
+
+// function draw() {
+//   background(1000);
+//   noStroke(2);
+//   fill(255, 0, 0);
+//   ellipse(width / 2, height / 2, 200, 200);
+
+//   circle.style.backgroundColor = color;
+// }
+
+//----------------------------------------------------------------------------
+let rectX = 0;
+let fr = Math.random(100) + 50 * 100; //starting FPS
+let clr;
 
 function setup() {
-  createCanvas(720, 400);
-  console.log("Hello!");
-
-  // Create a circle using vectors pointing from center
-  for (let angle = 0; angle < 360; angle += 9) {
-    // Note we are not starting from 0 in order to match the
-    // path of a circle.
-    let v = p5.Vector.fromAngle(radians(angle - 135));
-    v.mult(100);
-    circle.push(v);
-    // Let's fill out morph ArrayList with blank PVectors while we are at it
-    morph.push(createVector());
-  }
-
-  // A square is a bunch of vertices along straight lines
-  // Top of square
-  for (let x = -50; x < 50; x += 10) {
-    square.push(createVector(x, -50));
-  }
-  // Right side
-  for (let y = -50; y < 50; y += 10) {
-    square.push(createVector(50, y));
-  }
-  // Bottom
-  for (let x = 50; x > -50; x -= 10) {
-    square.push(createVector(x, 50));
-  }
-  // Left side
-  for (let y = 50; y > -50; y -= 10) {
-    square.push(createVector(-50, y));
-  }
+  createCanvas(600, 400);
+  background(200);
+  frameRate(fr); // Attempt to refresh at starting FPS
+  clr = color(
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256)
+  );
 }
 
 function draw() {
-  background(51);
+  background(200);
+  rectX += 1; // Move Rectangle
 
-  // We will keep how far the vertices are from their target
-  let totalDistance = 0;
+  if (rectX >= width) {
+    let rng = Math.floor(Math.random() * 256);
+    let rng2 = Math.floor(Math.random() * 256);
+    let rng3 = Math.floor(Math.random() * 256);
 
-  // Look at each vertex
-  for (let i = 0; i < circle.length; i++) {
-    let v1;
-    // Are we lerping to the circle or square?
-    if (state) {
-      v1 = circle[i];
+    // If you go off screen.
+    if (fr === 30) {
+      clr = color(0, 0, 255);
+      fr = Math.floor(Math.random(45, 50) + 2 * 185);
+      frameRate(fr); // make frameRate 10 FPS
     } else {
-      v1 = square[i];
+      clr = color(rng, rng2, rng3);
+      fr = Math.floor(Math.random(45, 50) + 2 * 185);
+      frameRate(fr); // make frameRate 30 FPS
     }
-    // Get the vertex we will draw
-    let v2 = morph[i];
-    // Lerp to the target
-    v2.lerp(v1, 0.1);
-    // Check how far we are from target
-    totalDistance += p5.Vector.dist(v1, v2);
+    rectX = 0;
   }
+  fill(clr);
+  rect(rectX, 40, 50, 50);
+}
 
-  // If all the vertices are close, switch shape
-  if (totalDistance < 0.1) {
-    state = !state;
+//----------------------------------------------------------------------------
+let rectX2 = 5;
+let fr2 = Math.random(100) + 50 * 100; //starting FPS
+let clr2;
+
+function setup() {
+  createCanvas(600, 400);
+  background(200);
+  frameRate(fr2); // Attempt to refresh at starting FPS
+  clr2 = color(
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256)
+  );
+}
+
+function draw() {
+  //background(200);
+  rectX2 += 1; // Move Rectangle
+
+  if (rectX2 >= width) {
+    let rng = Math.floor(Math.random() * 256);
+    let rng2 = Math.floor(Math.random() * 256);
+    let rng3 = Math.floor(Math.random() * 256);
+
+    // If you go off screen.
+    if (fr2 === 30) {
+      clr2 = color(0, 0, 255);
+      fr2 = Math.floor(Math.random(45, 50) + 2 * 185);
+      frameRate(fr2); // make frameRate 10 FPS
+    } else {
+      clr2 = color(rng, rng2, rng3);
+      fr2 = Math.floor(Math.random(45, 50) + 2 * 185);
+      frameRate(fr2); // make frameRate 30 FPS
+    }
+    rectX2 = 0;
   }
-
-  // Draw relative to center
-  translate(width / 2, height / 2);
-  strokeWeight(4);
-  // Draw a polygon that makes up all the vertices
-  beginShape();
-  noFill();
-  stroke(255);
-
-  morph.forEach((v) => {
-    vertex(v.x, v.y);
-  });
-  endShape(CLOSE);
+  fill(clr2);
+  rect(rectX2, 130, 90, 90);
 }
