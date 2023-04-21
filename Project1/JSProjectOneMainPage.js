798fe644245b3a6d42e524e49daa7d95dfd69e38
Nedu Ibezim <Ibezim.5@osu.edu>
2023-04-21 16:19:05 -0400

    Mach 1

--- Project1/JSProjectOneMainPage.js
+++ Project1/JSProjectOneMainPage.js
@@ -1,11 +1,14 @@
 var element = document.getElementById("EleTest");
 element.style.left = "100px";
-element.style.top = "50px"
+element.style.top = "50px";
 
-
-div = document.createElement('div');
+div = document.createElement("div");
 document.body.appendChild(div);
-div.className = 'fadein';
+div.className = "fadein";
 window.setTimeout(function () {
-  div.className = 'fadeout';
+  div.className = "fadeout";
 }, 3000);
+
+var el = document.getElementById("container");
+el.style.top = "150px";
+el.style.left = "100px";
